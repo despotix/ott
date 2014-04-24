639625b6450588e52252b30a6bf17ebede79c120
despotix <despotix@despotix-AOD270.(none)>
2014-04-25 00:13:59 +0300

    bin

---
+++ bin/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ..
+npm install express@3.0.0rc2
+npm install redis
+npm install date-utils
+npm install node-uuid
+npm install request
---
+++ bin/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ..
+foreverd -l ./logs/f.log -o ./logs/o.log -e ./logs/e.log start ./index.js
